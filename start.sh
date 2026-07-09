c756472b27faed5804d8b4630579b72dbfc75d2c
Pycukvsu <kha4etlov.ruslan@yandex.ru>
2026-07-09 16:29:26 +0300

    fix: start.sh properly parse postgresql:// URL with password containing special chars

--- start.sh
+++ start.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
 if [ -n "$SPRING_DATASOURCE_URL" ] && echo "$SPRING_DATASOURCE_URL" | grep -q "^postgresql://"; then
-    SPRING_DATASOURCE_URL="jdbc:$SPRING_DATASOURCE_URL"
-    export SPRING_DATASOURCE_URL
+    URL="$SPRING_DATASOURCE_URL"
+    URL="${URL#postgresql://}"
+
+    USER=$(echo "$URL" | cut -d: -f1)
+    REST=$(echo "$URL" | cut -d: -f2-)
+
+    PASS=$(echo "$REST" | cut -d@ -f1)
+    HOST_DB=$(echo "$REST" | cut -d@ -f2-)
+
+    HOST=$(echo "$HOST_DB" | cut -d/ -f1)
+    DB=$(echo "$HOST_DB" | cut -d/ -f2)
+
+    JDBC_URL="jdbc:postgresql://${HOST}/${DB}?user=${USER}&password=${PASS}"
+    export SPRING_DATASOURCE_URL="$JDBC_URL"
 fi
 
 exec java -jar app.jar --spring.profiles.active=prod
